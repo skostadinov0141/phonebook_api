9e4769b58b9133e7071648fa20662689ee1086e9
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2022-07-25 20:38:38 +0200

    added dockerfile and pushed image to dockerhub

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,19 @@
-FROM python
+FROM python:latest
+
+ARG SSL_KEYFILE
+ARG SSL_CERTFILE
+ARG API_PORT=8000
+ARG API_ADDRESS=127.0.0.1
+
+ENV MONGO_ADDRESS=127.0.0.1
+ENV MONGO_PORT=27017
+
+WORKDIR /home
+
+COPY . .
+
+RUN pip install uvicorn
+
+CMD [ "python", "uvicorn", "main:app", "--host=${API_ADDRESS}", "--port=${API_PORT}", "--ssl-keyfile=${SSL_KEYFILE}", "--ssl-certfile=${SSL_CERTFILE}"]
+
+EXPOSE ${API_PORT}
--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+from pprint import pprint
+import os
 
+# Import the phonebook model from the models directory
+from models.phonebook_entry import PhoneBookEnrty
+
+# Create a mongo client and connect it to the DB
+client = MongoClient(os.environ["MONGO_ADDRESS"] + ":" + os.environ["MONGO_PORT"])
+
+db = client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+# Create a FastAPI app
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {'message':'Hello World!'}
+
+# Add a phonebook entry to the database
+@app.post("/create_phonebook_entry")
+async def createPhonebookEntry(entry : PhoneBookEnrty):
+    phonebookDB = client.phonebook
+    phonebookDB.entries.insert_one(entry.dict())
+    return entry.dict()
+
+# Get all phonebook entries contained within the database
+@app.get("/get_phonebook_entries")
+async def getPhonebookEntries():
+    returnList = []
+    for document in client.phonebook.entries.find():
+        returnList.append({
+            "name":document["name"],
+            "address":document["address"],
+            "phoneNumber":document["phoneNumber"],
+        })
+    return returnList
