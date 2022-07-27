6272e34b6479cae9abdbea94f4a075a460618837
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2022-07-27 22:32:43 +0200

    final commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pymongo import MongoClient
 from pprint import pprint
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 # Import the phonebook model from the models directory
@@ -17,20 +18,29 @@ pprint(serverStatusResult)
 # Create a FastAPI app
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {'message':'Hello World!'}
 
 # Add a phonebook entry to the database
 @app.post("/create_phonebook_entry")
-async def createPhonebookEntry(entry : PhoneBookEnrty):
+async def createPhonebookEntry(response:Response, entry : PhoneBookEnrty):
     phonebookDB = client.phonebook
     phonebookDB.entries.insert_one(entry.dict())
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return entry.dict()
 
 # Get all phonebook entries contained within the database
 @app.get("/get_phonebook_entries")
-async def getPhonebookEntries():
+async def getPhonebookEntries(response:Response):
     returnList = []
     for document in client.phonebook.entries.find():
         returnList.append({
@@ -38,4 +48,5 @@ async def getPhonebookEntries():
             "address":document["address"],
             "phoneNumber":document["phoneNumber"],
         })
-    return returnList
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return {"data":returnList}
